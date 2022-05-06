394ee7cb720b8b3a784a8b5c8c0ba22fd873b97a
Kobayashi <shun123k@gmail.com>
2022-05-06 16:56:19 +0900

    initialize

---
+++ src/main/java/garden/Garden.java
@@ -0,0 +1,13 @@
+public class Garden {
+
+	private Counter count;
+
+	private Turnstile[] turnstile;
+
+	private Counter counter;
+
+	public void main() {
+
+	}
+
+}
---
+++ src/main/java/garden/Turnstile.java
@@ -0,0 +1,13 @@
+public class Turnstile extends Thread {
+
+	private int count;
+
+	private Counter Counter;
+
+	private Counter counter;
+
+	public void run() {
+
+	}
+
+}
